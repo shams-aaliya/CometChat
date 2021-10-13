b6fb0eba09a89e145aff58db7738b170cf427cf3
shams-aaliya <shamsaaliya08@gmail.com>
2021-10-13 14:10:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {CometChatUI} from '../src/cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/CometChatUI/index'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>CometChat integrating using React UI Kit</h1>
+    <CometChatUI />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CometChat } from "@cometchat-pro/chat";
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const appID = "19551138d2d28ee0";
+const region = "US";
+const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build();
+CometChat.init(appID, appSetting).then(
+  () => {
+    console.log("Initialization completed successfully");
+    // You can now call login function.
+    ReactDOM.render(
+      <React.StrictMode>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+      </React.StrictMode>
+      ,document.getElementById('root')
+      );
+  },
+  error => {
+    console.log("Initialization failed with error:", error);
+    // Check the reason for error and take appropriate action.
+  }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
